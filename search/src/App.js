a8e28a1261dab55b85798fb60aa1d8dcfde116a0
anjalidhanjode28 <anjalidhanjode28@gmail.com>
2023-03-19 00:39:23 +0530

    basics

--- search/src/App.js
+++ search/src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react"
+import { Users } from './users';
+import { Table } from './Table';
+
+//////////////////////BASIC SEARCH
+
+// function App() {
+//   const [query,setQuery] = useState("")
+  
+//   //console.log(Users.filter(user => user.first_name.toLowerCase().includes("f")))
+//   return (
+//     <div className="App">
+//       <input type="text" className="search"
+//        onChange={(e) => setQuery(e.target.value)}
+//       placeholder="Search" />
+//       <ul className="list">
+//       {
+//         Users.filter((user) => 
+//         user.first_name.toLowerCase().includes(query)
+//         ).map((user) => (
+//           <li key={user.id} className="listItem">
+//           {user.first_name}
+//           </li>
+
+//         ))
+//       }
+//       </ul>
+//     </div>
+//   );
+// }
+
+
+///////////////////////SEARCH ON A DATATABLE
 
 function App() {
+  const [query,setQuery] = useState("")
+
+  const keys = ["first_name", "last_name", "email"]
+  console.log(Users[0]["email"])
+ const search = (data) => {
+  return data.filter((item) => 
+  // item.first_name.toLowerCase().includes(query) ||
+  // item.last_name.toLowerCase().includes(query) ||
+  // item.email.toLowerCase().includes(query)
+
+  keys.some(key => item[key].toLowerCase().includes(query))
+  )
+ }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input 
+       type="text" className="search"
+       onChange={(e) => setQuery(e.target.value)}
+      placeholder="Search" />
+      
+      <Table data={search(Users)}/>
     </div>
   );
 }
-
 export default App;
